f34a9283a9aa7329d2d6f82ecdb58f007ee63a24
ahmed <u31999@gmail.com>
2022-08-25 13:11:50 -0700

    Modifie the header for the mobile view

--- front-end/src/component/Header.js
+++ front-end/src/component/Header.js
@@ -12,6 +12,7 @@ const [newsIsOpen, setNewsIsOpen] = useState(false)
 const [eventIsOpen, setEventIsOpen] = useState(false)
 const [storeIsOpen, setStoreIsOpen] = useState(false)
 const [searchIsOpen, setSearchIsOpen] = useState(false)
+const [navMobile, setNavMobileIsOpen] = useState(false)
 const [fillNav, setFillNav] = useState(false)
 
 const scrollPosition = useScrollPosition();
@@ -24,7 +25,11 @@ useEffect(()=> {
 
 
   return (
-    <div className={!scrollPosition 
+    <Media>
+    {({ breakpoints, currentBreakpoint }) => 
+    breakpoints[currentBreakpoint] >= breakpoints.desktop 
+    ? ( 
+      <div className={!scrollPosition 
       ? 'd-flex align-items-center header'
       : `d-flex align-items-center header fillNav`}>
     <Logo />
@@ -103,6 +108,110 @@ useEffect(()=> {
         </nav>
         </Container>
     </div>
+    )
+    : (
+      <div className={!scrollPosition 
+      ? 'd-flex align-items-center header'
+      : `d-flex align-items-center header fillNav`}>
+    <Logo />
+        
+    <Container className='d-flex align-items-center justify-content-center right-menu'>
+      <Dropdown className='menu nav align-items-center' 
+      isOpen={navMobile} 
+      toggle={(e) => e.target.classList.contains('n-btn') && setNavMobileIsOpen(!navMobile)}>
+        <DropdownToggle className='text-light n-btn' >
+        Nav
+        </DropdownToggle>
+        <DropdownMenu>
+        <DropdownItem className='text-dark'>
+            <NavLink to='/' className='text-dark'>
+                Home
+            </NavLink>
+        </DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem className='text-dark'>
+             <Dropdown isOpen={newsIsOpen} toggle={() => setNewsIsOpen(!newsIsOpen)}>
+             
+                <DropdownToggle className='text-dark' >
+                  News {!newsIsOpen ? <FaAngleDown /> : <FaAngleUp />}
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem className='text-dark'>Header</DropdownItem>
+                  <DropdownItem divider/>
+                  <DropdownItem className='text-dark'>Action</DropdownItem>
+                  <DropdownItem divider/>
+                  <DropdownItem className='text-dark'>Another Action</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem className='text-dark'>Another Action</DropdownItem>
+                </DropdownMenu>
+          </Dropdown>
+        </DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>
+          <Dropdown isOpen={eventIsOpen} toggle={() => setEventIsOpen(!eventIsOpen)}>
+             
+                <DropdownToggle className='text-dark' >
+                    Events {!eventIsOpen ? <FaAngleDown /> : <FaAngleUp />}
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem className='text-dark'>Header</DropdownItem>
+                  <DropdownItem divider/>
+                  <DropdownItem className='text-dark'>Action</DropdownItem>
+                  <DropdownItem divider/>
+                  <DropdownItem className='text-dark'>Another Action</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem className='text-dark'>Another Action</DropdownItem>
+                </DropdownMenu>
+          </Dropdown>
+        </DropdownItem>
+          <DropdownItem divider />
+        <DropdownItem>
+          <Dropdown isOpen={storeIsOpen} toggle={() => setStoreIsOpen(!storeIsOpen)}>
+             
+                <DropdownToggle className='text-dark' >
+                    Store {!storeIsOpen ? <FaAngleDown /> : <FaAngleUp />}
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem className='text-dark'>Header</DropdownItem>
+                  <DropdownItem divider/>
+                  <DropdownItem className='text-dark'>Action</DropdownItem>
+                  <DropdownItem divider/>
+                  <DropdownItem className='text-dark'>Another Action</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem className='text-dark'>Another Action</DropdownItem>
+                </DropdownMenu>
+          </Dropdown> 
+        </DropdownItem>   
+        <DropdownItem divider />
+        <DropdownItem className='text-dark'>
+            <NavLink to='/sign-in'>
+                Sign In
+            </NavLink>
+        </DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>
+              <Dropdown isOpen={searchIsOpen} toggle={() => setSearchIsOpen(!searchIsOpen)} 
+              className='dropdown-search'>
+                <DropdownToggle className='text-dark btn-search' >
+                  {!searchIsOpen ? <FaSearch /> : <FaTimes className='indiciater-color'/>}
+                </DropdownToggle>
+                <DropdownMenu className='has-search'>
+                    <Form className='d-flex align-items-center'>
+                      <div className='d-flex align-items-center p-1'>
+                        <FaSearch className='form-control-feedback'/>
+                       <input type="text" className="form-control" placeholder="Search" />
+                      </div>
+                    </Form>
+                </DropdownMenu>
+              </Dropdown>
+        </DropdownItem>
+      </DropdownMenu>
+        </Dropdown>
+        </Container>
+    </div>
+    )
+    }
+    </Media>
   )
 }
 
